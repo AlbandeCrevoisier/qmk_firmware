78e2e1dc4690ad1f661977c29c7393c184950504
∇ <albandecrevoisier@gmail.com>
2024-11-27 14:52:11 +0100

    Swap ctrl & shift in SC layout

--- keyboards/splitkb/aurora/sweep/keymaps/ach_v2/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/ach_v2/keymap.c
@@ -25,21 +25,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F11, KC_TRNS, KC_TRNS,
                                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-	[_SC] = LAYOUT(KC_Q,                   KC_W, KC_E, KC_R,    KC_T,    KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
-                   KC_A,                   KC_S, KC_D, KC_F,    KC_G,    KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN,
-                   LM(_SC_CTRL, MOD_LCTL), KC_X, KC_C, KC_V,    KC_B,    KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
-                                                    KC_TRNS, MO(_SC_FN), KC_SPC, DF(_BASE)),
+	[_SC] = LAYOUT(KC_Q, KC_W, KC_E,                   KC_R, KC_T, KC_Y,   KC_U, KC_I,    KC_O,   KC_P,
+                   KC_A, KC_S, KC_D,                   KC_F, KC_G, KC_H,   KC_J, KC_K,    KC_L,   KC_SCLN,
+                   KC_Z, KC_X, KC_C, LM(_SC_CTRL, MOD_LCTL), KC_B, KC_N,   KC_M, KC_COMM, KC_DOT, KC_SLSH,
+                                              KC_TRNS, MO(_SC_FN), KC_SPC, DF(_BASE)),
 
 	[_SC_FN] = LAYOUT(KC_1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,          KC_8,   KC_9,    KC_0,
                       KC_EQL, KC_J,    KC_LALT, KC_BSPC, KC_CAPS, MS_BTN2, OSM(MOD_RGUI), KC_ENT, KC_BSLS, KC_MINS,
-                      KC_GRV, KC_RCTL, KC_Z,    KC_TAB,  KC_7,    MS_BTN1, KC_QUOT,       KC_ESC, KC_LBRC, KC_RBRC,
+                      KC_GRV, KC_RCTL, KC_V,    KC_TAB,  KC_7,    MS_BTN1, KC_QUOT,       KC_ESC, KC_LBRC, KC_RBRC,
                                                 KC_TRNS, KC_TRNS, KC_LSFT, KC_TRNS),
 
-    [_SC_CTRL] = LAYOUT(KC_TRNS, KC_2,    KC_3,    KC_Q,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                        KC_TRNS, KC_J,    KC_LALT, KC_W,    KC_A,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                        KC_TRNS, KC_RCTL, KC_Z,    KC_E,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-
+    [_SC_CTRL] = LAYOUT(KC_Q, KC_2,    KC_3,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                        KC_W, KC_J,    KC_LALT, KC_A,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                        KC_E, KC_RCTL, KC_V,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 #ifdef ENCODER_ENABLE
