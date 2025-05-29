80afee4ba5abe99f933bc2d9408c5b8ab0ed016d
ach <albandecrevoisier@gmail.com>
2025-05-29 19:27:45 +0200

    Turn off the on-board power LED

--- keyboards/splitkb/aurora/sweep/keymaps/ach_v2/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/ach_v2/keymap.c
@@ -1,4 +1,11 @@
 #include QMK_KEYBOARD_H
+#include "gpio.h"
+
+// Turn off the on-board power LED
+void keyboard_pre_init_user(void) {
+  gpio_set_pin_output(24);
+  gpio_write_pin_high(24);
+}
 
 enum layers {
 	_BASE,
